e5cbe76d7ab805a6e8f6b39e7c26b40c5a6b6966
Noah <nosherway@yahoo.com>
2021-05-26 19:36:34 -0500

    adding updated models

--- models/User.js
+++ models/User.js
@@ -1,68 +1,4 @@
-<<<<<<< HEAD
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  //     thoughts: [
-  //       {
-  //         type: Schema.Types.ObjectId,
-  //         ref: "Thought",
-  //       },
-  //     ],
-  //     friends: [
-  //       {
-  //         type: Schema.Types.ObjectId,
-  //         ref: "User",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     toJSON: {
-  //       virtuals: true,
-  //     },
-});
-
-// set up pre-save middleware to create password
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
-// userSchema.virtual("friendCount").get(function () {
-//   return this.friends.length;
-// });
-
-const User = model("User", userSchema);
-
-module.exports = User;
-=======
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
   username: String,
@@ -71,5 +7,4 @@ const userSchema = new Schema({
   createdAt: String,
 });
 
-module.exports = model('User', userSchema);
->>>>>>> 4fe5f60e6681592db0b348c38f13d1e5ef8c5e15
+module.exports = model("User", userSchema);
