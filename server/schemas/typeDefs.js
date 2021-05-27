2e1d84158397928135210c7d2982252186f1d29e
Nate Schroeder <n8dogg59@hotmail.com>
2021-05-26 21:23:02 -0500

    implemented authorization

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,11 +30,22 @@ type Post {
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     posts(username: String): [Post]
     post(_id: ID!): Post
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
   }
 `;
 
