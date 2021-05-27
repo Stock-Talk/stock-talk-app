3e4bfc8a7396a9e8544b4e8c96a0c622c5e82685
pepermz <74684215+pepermz@users.noreply.github.com>
2021-05-27 10:59:10 -0500

    login page and registration

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from 'react'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+const LoginForm = () => (
+  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as='h2' color='teal' textAlign='center'>
+        <Image src='/logo.png' /> Log-in to your account
+      </Header>
+      <Form size='large'>
+        <Segment stacked>
+          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+          />
 
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
-}
+          <Button color='teal' fluid size='large'>
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <Link to="/register">Sign Up</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+)
 
-export default Login;
+export default LoginForm
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,42 @@
 import React from 'react';
-
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' textAlign='center'>
+          <Image src='/logo.png' /> Create your account
+      </Header>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Confirm Password'
+              type='password'
+            />
+
+            <Button color='teal' fluid size='large'>
+              Register
+          </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Already have an account? <Link to="/login">Login</Link>
+        </Message>
+      </Grid.Column>
+
+    </Grid>
   );
 }
 
